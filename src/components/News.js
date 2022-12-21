a8840cb7075d01027a1f36bdc09db1b3fe7ed30d
Yash <yashdahekar92@gmail.com>
2022-12-22 02:19:35 +0530

    3

--- src/components/News.js
+++ src/components/News.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  render() {
-    return (
-      <div>
-        This is a news Component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='container my-3'>
+                <h2>NewsBuddy - Top Headlines</h2>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title='my title' description='my descrip' />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title='my title' description='my descrip' />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title='my title' description='my descrip' />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-  render() {
-    return (
-      <div>
-        This is a news item2
-      </div>
-    )
-  }
+    render() {
+        let {title, description} = this.props;
+        return (
+            <div>
+                <div className="card" style={{width: "18rem"}}>
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href="/" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default NewsItem
